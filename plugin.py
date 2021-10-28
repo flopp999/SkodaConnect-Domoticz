d7ba0add2425d4add338033706692d1528b46a67
flopp999 <danielnilsson79@hotmail.com>
2021-10-28 22:49:12 +0200

    typo

--- plugin.py
+++ plugin.py
@@ -294,7 +294,7 @@ def UpdateDeviceCharge(name, sValue):
         unit = "kW"
         UpdateDevice(name, sValue, Pass, ID, unit, Description)
         ID = 77
-        name = "Charge rate/h %"
+        name = "Charge rate %/h"
         sValue = float(sValue) / int(_plugin.Battery) * 100
         unit = "%/h"
 
